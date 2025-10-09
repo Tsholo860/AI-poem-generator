b19475eb92073a533d347ba49db96405c76e3b32
Tsholo860 <tsholo860@gmail.com>
2025-10-09 19:12:28 +0200

    updated and added new function to JS file

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
-function generatePoem(event){
-    event.preventDefault();
-     
-    
-    new Typewriter("#poem", {
-        strings:"Rose are red and violets are blue ",
+function displayPoem(response){
+
+new Typewriter("#poem", {
+        strings: response.data.answer,
         autoStart:true,
         delay:1,
         cursor:"",
     } );
+
+}
+
+
+function generatePoem(event){
+    event.preventDefault();
+
+    let instructionsInput = document.querySelector("#user-instructions"); 
+    let apiKey = '01f1e3c8846fa60fce9eodfad5fbt6b4';
+    let prompt = `User instructions: Generate a poem ${instructionsInput.value}`;
+    let context = "You are a poet and love short but meaningful poems. You are tto generate a 6 line poem in basic HTML and seperate each line with a <br/>  Make sure to follow the user instructions.";
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+     axios.get(apiUrl).then(displayPoem);
+    
+    
     
 }
 
