dbcc84a60aec90a27264951252d8c4e91d9e590a
Tsholo860 <tsholo860@gmail.com>
2025-10-17 20:32:42 +0200

    edited new function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 function displayPoem(response){
 
-new Typewriter("#poem", {
+ new Typewriter("#poem", {
         strings: response.data.answer,
         autoStart:true,
         delay:1,
@@ -15,11 +15,11 @@ function generatePoem(event){
 
     let instructionsInput = document.querySelector("#user-instructions"); 
     let apiKey = '01f1e3c8846fa60fce9eodfad5fbt6b4';
-    let prompt = `User instructions: Generate a poem ${instructionsInput.value}`;
-    let context = "You are a poet and love short but meaningful poems. You are tto generate a 6 line poem in basic HTML and seperate each line with a <br/>  Make sure to follow the user instructions.";
+    let prompt = `User instructions: Generate a poem  ${instructionsInput.value}`;
+    let context = "Generate a 4 line poem with each line on a new <br/>";
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-     axios.get(apiUrl).then(displayPoem);
+    axios.get(apiUrl).then(displayPoem);
     
     
     
@@ -27,5 +27,5 @@ function generatePoem(event){
 
 
 let poemFormElement = document.querySelector("#poem-generator");
-poemFormElement.addEventListener('submit',generatePoem);
+poemFormElement.addEventListener("submit",generatePoem);
 
